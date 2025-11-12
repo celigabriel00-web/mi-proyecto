0fbf9d8ff2abeb3229cdb54f703f71406108f92d
celigabriel00-web <celigabriel00@gmail.com>
2025-11-12 06:39:59 -0500

    feat: actualizacion backend a mi repositorio

--- src/backend/swagger.js
+++ src/backend/swagger.js
@@ -1,23 +1,24 @@
 import swaggerJsdoc from "swagger-jsdoc";
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: { 
-      title: "API REST - Proyect streamingfilm",
-    version: "1.0.0",
-    description: "Documentación de API REST con Express, Prisma y Postman.",
-    contact:{
-        autor: 'Isaac Pérez',
-        email: '1627141@senati.edu.pe',
-        telephone: '+51 983569753'
-    }
-},
+      title: "API REST - Proyecto streamingfilm",  // Corregido para que coincida con la convención
+      version: "1.0.0", // Versión de la API
+      description: "Documentación de API REST con Express, Prisma y Postman.", // Descripción de la API
+      contact: {
+        author: 'Oscar Celi',  // Cambié 'autor' a 'author' para ajustarse al estándar Swagger
+        email: '1641296@senati.pe',
+        telephone: '+51 970263300'
+      }
+    },
     servers: [{
-        url: "http://localhost:3000",
-        description: "Servidor de desarrollo"
+      url: "http://localhost:3000", // URL del servidor de desarrollo
+      description: "Servidor de desarrollo"
     }],
   },
-  apis: ["./routes/*.js"],
+  apis: ["./routes/*.js"],  // Ruta para buscar los archivos de las rutas
 };
-export const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
+export const swaggerSpec = swaggerJsdoc(swaggerOptions);
